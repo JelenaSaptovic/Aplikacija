9f1775e57a6aeaab7ae9adc7fbdec3f9a6f3b860
Jelena Šaptović <jelena.saptovic.18@singimail.rs>
2022-06-14 17:34:19 +0200

    Extend Request object with user data

--- 03-back-end/src/middlewares/AuthMiddleware.ts
+++ 03-back-end/src/middlewares/AuthMiddleware.ts
@@ -31,6 +31,8 @@ export default class AuthMiddleware {
                 }
             }
 
+            req.authorisation = check [0];
+
             next();
         } catch (error){
             res.status(error?.status ?? 500).send(error?.message);   
