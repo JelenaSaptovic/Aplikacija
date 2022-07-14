9681b4df98ce98386ed607e19f862aa647c878f5
JelenaSaptovic <jelena.saptovic.18@singimail.rs>
2022-07-14 09:01:23 -0700

    Return old access control

--- 03-back-end/src/components/user/UserController.controller.ts
+++ 03-back-end/src/components/user/UserController.controller.ts
@@ -31,9 +31,9 @@ class UserController extends BaseController {
         const id: number = +req.params?.id;
 
         //samo korisnik sa datim id može da vidi informacije o sebi
-        //if (req.authorisation?.userId !== id){
-         //   return res.status(403).send("You are not authorised to access this resource.!");
-        //}
+        if (req.authorisation?.userId !== id){
+            return res.status(403).send("You are not authorised to access this resource.!");
+        }
 
         this.services.user.getById(id, {
             loadAd: true
@@ -254,7 +254,7 @@ class UserController extends BaseController {
         if (req.authorisation?.userId !== id){
             return res.status(403).send("You are not authorised to access this resource.!");
         }
-
+        
         this.services.user.getById(id, { loadAd: false })
             .then(result => {
                 if (result === null){
